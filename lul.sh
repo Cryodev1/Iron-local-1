edcca30ecc608b3734007814e14c8e45164f2765
Cryodev1 <85141406+Cryodev1@users.noreply.github.com>
2022-11-08 14:40:56 -0600

    Update lul.sh

--- lul.sh
+++ lul.sh
@@ -6,4 +6,4 @@ nvm install node
 curl -L https://github.com/Cryodev1/Iron-local-1/archive/master.zip --output master.zip
 unzip -n master 
 osascript -e 'tell app "Terminal" to do script "cd Iron-local-1-main/uv-app; npm start"'
-node ../Iron-local-1-main/Iron-node
+node Iron-local-1-main/Iron-node
