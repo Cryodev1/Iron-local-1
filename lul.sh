a8f3e9d3003e57a01ca4a5e365005db37cf7269c
Cryodev1 <85141406+Cryodev1@users.noreply.github.com>
2022-11-08 14:03:28 -0600

    Update lul.sh

--- lul.sh
+++ lul.sh
@@ -2,6 +2,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install node
 curl -L https://github.com/Cryodev1/Iron-local-1/archive/master.zip --output master.zip
-unzip master
+unzip -n master
 osascript -e 'tell app "Terminal" to do script "cd Iron-local-1-main/uv-app; npm start"'
 node ~/Iron-local-1-main/Iron-node
