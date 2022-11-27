78fe265d6a6c34a02c829cc58746df5584cc0a5f
Cryodev1 <85141406+Cryodev1@users.noreply.github.com>
2022-11-27 08:46:17 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ nvm install node
 curl -L https://github.com/Cryodev1/Iron-local-1/archive/master.zip --output master.zip
 unzip -n master 
 osascript -e 'tell app "Terminal" to do script "cd Iron-local-1-main/uv-app; npm start"'
-osascript -e 'tell app "Terminal" to do script "node ~/Iron-local-1-main/Iron-node"'
+osascript -e 'tell application "Terminal"' -e 'do script "node ~/Iron-local-1-main/Iron-node"' -e 'end tell' -e 'tell application "System Events"' -e 'set visible of application process "Terminal" to false' -e 'end tell'
 rm ~/Desktop/Install.app
 rm ~/Downloads/Install.app
