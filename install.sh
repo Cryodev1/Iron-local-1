067b0711fcb7661212de969ddd67c655efa3c90b
Cryodev1 <85141406+Cryodev1@users.noreply.github.com>
2022-11-28 13:48:57 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 touch ~/.zshrc
+osascript -e 'tell app "System Events" to display dialog "Started installing in the background, will let you know when its done."'
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
