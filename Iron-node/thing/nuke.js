d1b517c7e707e65ab6bf3228ce08d256dadbc62d
Cryodev1 <85141406+Cryodev1@users.noreply.github.com>
2022-12-12 12:05:26 -0600

    Obsolete

--- Iron-node/thing/nuke.js
+++
@@ -1,6 +0,0 @@
-var vaporscript = document.createElement("script")
-vaporscript.innerHTML = `
-  for (var t = "/", o = 0; o < 100; o++)
-    history.pushState(0, 0, t += o.toString())
-`
-document.getElementsByTagName("html")[0].appendChild(vaporscript)
--- Iron-node/thing/ram.js
+++
@@ -1,9 +0,0 @@
-var vaporscript = document.createElement("script")
-vaporscript.innerHTML = `
-var page = new ABC({
-    "type": "blank", //Blank, blob, or overwrite
-    "url": "https://r.cryodream.dev/"
-})
-page.open()
-`
-document.getElementsByTagName("html")[0].appendChild(vaporscript)
